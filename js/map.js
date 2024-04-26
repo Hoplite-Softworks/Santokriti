ce343fecc1809805ee89785759ea52748e6d7a24
Panos Lelakis <plelakis@gmail.com>
2024-04-26 14:36:55 +0300

    First official commit

---
+++ js/map.js
@@ -0,0 +1,12 @@
+const checkboxes = document.querySelectorAll('input');
+const images = document.querySelectorAll('.places');
+
+checkboxes.forEach(function(checkbox, index) {
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) { 
+            images[index].style.display = 'block';
+        } else {
+            images[index].style.display = 'none';
+        }
+    });
+});
