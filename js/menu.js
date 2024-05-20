d2a032bf0b1c1f21d20b7553c7bf04e54358ce34
Panos Lelakis <plelakis@gmail.com>
2024-05-20 20:41:29 +0300

    Struct + db organized

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleContent(categoryId) {
-    var content = document.getElementById(categoryId + "-content");
-    if (content.style.display === "none") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
